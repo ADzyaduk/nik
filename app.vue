dc2c8814349c55ac374cf9a20c0c67e21da5d7f6
Alex Dz <avdzyaduk@gmail.com>
2024-03-06 16:01:33 +0300

    06031

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
-
+    <BtnPhonebtn />
     <UNotifications icon="i-heroicons-check-badge" color="primary" :id="6" title="Notification"
       description="This is a notification." :timeout="600000" />
   </div>
